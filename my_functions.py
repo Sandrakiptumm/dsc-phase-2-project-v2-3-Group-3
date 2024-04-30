75036896dbe9af0e6f9dfae92c282dd652fe85ca
Salahudin654 <salahudin.mahamed@student.moringaschool.com>
2024-04-30 22:30:55 +0300

    plotting Multiple linear Regression

--- my_functions.py
+++ my_functions.py
@@ -5,11 +5,13 @@ def building_model(formula, data_frame):
     return (sm.formula.ols(formula= formula, data = data_frame))
 
 
+
 def plotting_regression_plots(your_model, independent_variable):
     """
     Plots all necessary regression plots
     """
     fig, ax = plt.subplots(figsize=(8, 6))  # Create a figure and axis
-    sm.graphics.plot_regress_exog(your_model, independent_variable, ax=ax)  # Plot regression plots on the axis
+    sm.graphics.plot_regress_exog(your_model, independent_variable, fig=fig)  # Plot regression plots on the axis
     ax.set_xlabel(independent_variable, color='orange')  # Set x-axis label color to orange
     ax.tick_params(axis='x', colors='orange')  # Set x-axis tick color to orange
+    
